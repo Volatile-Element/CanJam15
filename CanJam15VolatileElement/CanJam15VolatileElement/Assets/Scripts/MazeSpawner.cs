850b921f2d6b3fd52f7e61afc5fa3dba4f54eb34
Ashley Blake-Hood <Ashley Blake-Hood>
2015-03-14 22:48:16 +0000

    Fixed the fix? Github too stronk 4 u

--- CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/MazeSpawner.cs
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/MazeSpawner.cs
@@ -3,46 +3,131 @@ using System.Collections;
 
 public class MazeSpawner : MonoBehaviour
 {
-	public GameObject straight;
-	public GameObject corner;
-	public GameObject junction;
+    public GameObject straight;
+    public GameObject flickerLight;
+    public GameObject wallPickup;
+    public GameObject cornerPickup;
+    public GameObject startPoint;
+    public GameObject endPoint;
+    public GameObject player;
 
-	int[,] mazeBase;
+    public PartManager partManager;
+
+    int[,] mazeBase;
+
+    public int wallPickupLimit;
+    public int cornerPickupLimit;
+    public int flickeringLightLimit;
+    public int height;
+    public int width;
+
+    bool hasStart;
+    bool hasEnd;
+
+    // Use this for initialization
+    void Start()
+    {
+        //int height = 46;
+        //int width = 46;
+        //int seed = Random.seed;
+        int seed = 125;
+        Random.seed = seed;
+        MazeGenerator mazeGenerator = new MazeGenerator();
+
+        mazeBase = mazeGenerator.CreateMaze(seed, width, height, 10);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject spawnedWall;
+
+                if (mazeBase[x, y] > 0)
+                {
+                    spawnedWall = (GameObject)Instantiate(straight, new Vector3(x, 0.5f, y), Quaternion.identity);
+
+                    if (mazeBase[x, y] == 1)
+                    {
+                        partManager.listStraights.Add(spawnedWall);
+                        spawnedWall.GetComponent<Renderer>().material.color = partManager.colorStraights;
+                        spawnedWall.transform.parent = gameObject.transform;
+                    }
+                    else if (mazeBase[x, y] == 2)
+                    {
+                        spawnedWall.GetComponent<Renderer>().material.color = partManager.colorCorners;
+                        spawnedWall.name = "Corner";
+                        partManager.listCorners.Add(spawnedWall);
+                        spawnedWall.transform.parent = gameObject.transform;
+                    }
+                    else if (mazeBase[x, y] == 3)
+                    {
+                        spawnedWall.GetComponent<Renderer>().material.color = partManager.colorStraights;
+                        spawnedWall.name = "Straight";
+                        partManager.listStraights.Add(spawnedWall);
+                        spawnedWall.transform.parent = gameObject.transform;
+                    }
+                }
+                else
+                {
+                    if (!hasStart)
+                    {
+                        spawnedWall = (GameObject)Instantiate(startPoint, new Vector3(x, 0.5f, y), Quaternion.identity);
+                        spawnedWall.name = "Start Point";
+                        hasStart = true;
+                        player.transform.position = spawnedWall.transform.position;
+                        spawnedWall.transform.parent = gameObject.transform;
+                        continue;
+                    }
+                    else if (Random.Range(0, 1000) > 990 && flickeringLightLimit > 0)
+                    {
+                        spawnedWall = (GameObject)Instantiate(flickerLight, new Vector3(x, 1f, y), Quaternion.identity);
+                        spawnedWall.name = "Flicker Light";
+                        flickeringLightLimit--;
+                        spawnedWall.transform.parent = gameObject.transform;
+                        continue;
+                    }
+                    else if (Random.Range(0, 1000) > 990 && wallPickupLimit > 0)
+                    {
+                        spawnedWall = (GameObject)Instantiate(wallPickup, new Vector3(x, 0.5f, y), Quaternion.identity);
+                        spawnedWall.name = "Wall Pickup";
+                        wallPickupLimit--;
+                        spawnedWall.transform.parent = gameObject.transform;
+                        continue;
+                    }
+                    else if (Random.Range(0, 1000) > 990 && cornerPickupLimit > 0)
+                    {
+                        spawnedWall = (GameObject)Instantiate(cornerPickup, new Vector3(x, 0.5f, y), Quaternion.identity);
+                        spawnedWall.name = "Corner Pickup";
+                        cornerPickupLimit--;
+                        spawnedWall.transform.parent = gameObject.transform;
+                        continue;
+                    }
+                }
 
 
-	// Use this for initialization
-	void Start ()
-	{
-		int height = 50;
-		int width = 50;
+            }
+        }
 
-		MazeGenerator mazeGenerator = new MazeGenerator ();
+        for (int x = width - 1; x > 0; x--)
+        {
+            for (int y = height - 1; y > 0; y--)
+            {
+                GameObject endSpawn;
 
-		mazeBase = mazeGenerator.CreateMaze (2077, width, height, 10);
+                if (mazeBase[x, y] == 0 && !hasEnd)
+                {
+                    endSpawn = (GameObject)Instantiate(endPoint, new Vector3(x, 0.5f, y), Quaternion.identity);
+                    endSpawn.name = "End Point";
+                    hasEnd = true;
+                    break;
+                }
+            }
+        }
+    }
 
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				GameObject spawnedWall;
+    // Update is called once per frame
+    void Update()
+    {
 
-				if (mazeBase[x, y] > 0)
-				{
-					spawnedWall = (GameObject)Instantiate (straight, new Vector3(x, 0, y), Quaternion.identity);
-
-					if (mazeBase[x,y] == 1)
-						spawnedWall.GetComponent<Renderer>().material.color = Color.red;
-					else if (mazeBase[x,y] == 2)
-						spawnedWall.GetComponent<Renderer>().material.color = Color.green;
-					else if (mazeBase[x,y] == 3)
-						spawnedWall.GetComponent<Renderer>().material.color = Color.blue;
-				}
-			}
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
+    }
 }
