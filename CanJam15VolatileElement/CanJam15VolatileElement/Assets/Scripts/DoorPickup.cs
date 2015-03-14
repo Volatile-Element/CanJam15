f99c738cf7ac0a65cfe9c29a761c05499f777588
Peter Keen <peterfkeen@hotmail.com>
2015-03-14 15:42:02 +0000

    Base classes + base tracing

---
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/DoorPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorPickup : PickupAbstract
+{
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.name == "Player") 
+		{
+			Color testColor = new Color (0, 100, 0, 1);
+			ChangeColor (testColor);
+			//destroyObject ();
+		}
+	}
+	public override void Triggers()
+	{
+		Debug.Log ("Reached");
+	}
+}
