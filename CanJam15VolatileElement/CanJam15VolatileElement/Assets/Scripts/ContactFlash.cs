b8f9e91c08c0cad26243b2ec4e8939162661874d
Ashley Blake-Hood <Ashley Blake-Hood>
2015-03-14 19:39:40 +0000

    Some sound work and prefab updates

---
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/ContactFlash.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContactFlash : MonoBehaviour {
+
+    MeshRenderer mr;
+
+	// Use this for initialization
+	void Start () {
+        mr = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+
+    void OnCollisionExit(Collision c)
+    {
+        mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+    }
+}
---
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetCameraColour(Color value)
+    {
+        mainCamera.backgroundColor = value;
+    }
+}
