b76d5adce20a6e5d71ea3a92aa6bfc61cafd4d7a
Ashley Blake-Hood <Ashley Blake-Hood>
2015-03-14 23:55:54 +0000

    Fixed manager duplication

---
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+    GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = FindObjectOfType<GameManager>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
