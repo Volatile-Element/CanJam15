7595d3915aa7dfa3daf8a9d592ae5c72a47f5d9a
Jordan Bird <jordanbird-29@hotmail.co.uk>
2015-03-15 05:47:10 +0000

    Teleporters Added
    Just needs to be added to the parts manager and will work.

---
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/TeleporterObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleporterObject : MonoBehaviour
+{
+	TeleporterManager teleporterManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		teleporterManager = FindObjectOfType<TeleporterManager> ();
+		teleporterManager.AddTeleporter (this);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.GetComponent<CharControler>() != null)
+		{
+			teleporterManager.TeleportPlayer (this, other.gameObject);
+		}
+	}
+
+	public void DestroyTeleporter()
+	{
+		//Spawn Use Prefab
+		Instantiate (teleporterManager.usedParticleBurst, transform.position, Quaternion.identity);
+
+		//TODO: Play sound.
+
+		Destroy (gameObject);
+	}
+}
