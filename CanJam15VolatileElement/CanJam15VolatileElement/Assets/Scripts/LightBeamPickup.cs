a42ca9744ee1e0cb88bc9daeabd1a6d87c51c9b2
Jordan Bird <jordanbird-29@hotmail.co.uk>
2015-03-15 00:39:10 +0000

    Started Adding Light Beam Pickup

---
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/LightBeamPickup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBeamPickup : PickupAbstract
+{
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.name == "Player") 
+		{
+			collision.gameObject.AddComponent<BeamScatter>();
+		}
+	}
+
+	public override void Triggers()
+	{
+		Debug.Log ("Ok");
+		//partManager.NewSurface(PartManager.Showing.LightBeam);
+	}
+}
--- CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/Pickups/BeamScatter.cs
+++ CanJam15VolatileElement/CanJam15VolatileElement/Assets/Scripts/Pickups/BeamScatter.cs
@@ -5,22 +5,33 @@ public class BeamScatter : MonoBehaviour
 {
 	public GameObject orbsToThrow;
 
+	public int maxUses = 5;
+	public int uses = 0;
+
 	// Use this for initialization
-	void Start () {
-		ThrowOrbs (1);
+	void Start ()
+	{
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			ThrowOrbs (1);
 	}
 
 	public void ThrowOrbs(int amount)
 	{
+		uses++;
+
 		for (int i = 0; i < amount; i++)
 		{
 			GameObject orb = (GameObject)Instantiate(orbsToThrow, transform.position, Quaternion.identity);
 			orb.GetComponent<Rigidbody>().AddForce (transform.forward * 150);
 		}
+
+		if (uses > maxUses)
+			Destroy (this);
 	}
 }
